08190211a8e0ee759ab3f643a4739542562e6c03
Tim Fontes <italo5123@hotmail.com>
2023-02-21 01:15:21 -0300

    feat: :sparkles: draw gui bg

--- core/game.go
+++ core/game.go
@@ -11,7 +11,8 @@ type GameState struct {
 }
 
 type Game struct {
-	state GameState
+	state           GameState
+	previousGamepad byte
 }
 
 func NewGame() Game {
@@ -32,8 +33,16 @@ func (s *Game) Setup() {
 }
 
 func (s *Game) Update() {
-	s.Setup()
-	s.DrawStats()
+	var gamepad = *w4.GAMEPAD1
+	var pressedThisFrame = gamepad & (gamepad ^ s.previousGamepad)
+	s.previousGamepad = gamepad
+
+	if pressedThisFrame&w4.BUTTON_LEFT != 0 {
+
+	}
+
+	s.drawGUIBg()
+	s.drawStats()
 }
 
 func (s *Game) restart() {
@@ -41,14 +50,30 @@ func (s *Game) restart() {
 	s.state.gameOver = false
 }
 
-func (s *Game) DrawStats() {
-	/* *w4.DRAW_COLORS = 4 */
+func (s *Game) drawStats() {
+	*w4.DRAW_COLORS = 4
 	w4.Text("LEVEL", 104, 16)
 	w4.Text("SCORE", 104, 40)
 	w4.Text("BEST", 104, 64)
 
-	/* *w4.DRAW_COLORS = 1 */
+	*w4.DRAW_COLORS = 1
 	w4.Text("2", 104, 24+2)
 	w4.Text("3", 104, 48+2)
 	w4.Text("4", 104, 72+2)
+}
+
+func (s *Game) drawGUIBg() {
+	*w4.DRAW_COLORS = 3
+	w4.Rect(0, 0, BOARD_OFFSET_X, 160)
+	w4.Rect(BOARD_OFFSET_X+BOARD_WIDTH*8, 0, 160-(BOARD_OFFSET_X+BOARD_WIDTH*8), 160)
+
+	*w4.DRAW_COLORS = 4
+	w4.Rect(BOARD_OFFSET_X, 0, 1, 160)
+	w4.Rect(BOARD_OFFSET_X+BOARD_WIDTH*8-1, 0, 1, 160)
+}
+
+func (s *Game) drawGameoverText() {
+	*w4.DRAW_COLORS = 4
+	w4.Text("GAME OVER", BOARD_OFFSET_X+4, 64)
+	w4.Text(" PRESS X", BOARD_OFFSET_X+4, 64+8+2)
 }
--- main.go
+++ main.go
@@ -1,31 +1,19 @@
 package main
 
 import (
-	"cart/w4"
+	"cart/core"
 )
 
-/* var gameInstance = core.NewGame() */
+var gameInstance = core.NewGame()
 
-var smiley = [8]byte{
-	0b11000011,
-	0b10000001,
-	0b00100100,
-	0b00100100,
-	0b00000000,
-	0b00100100,
-	0b10011001,
-	0b11000011,
+//go:export start
+func start() {
+	gameInstance.Setup()
 }
 
+//go:export update
 func update() {
-	*w4.DRAW_COLORS = 2
-	w4.Text("Hello from Go!", 10, 10)
 
-	var gamepad = *w4.GAMEPAD1
-	if gamepad&w4.BUTTON_1 != 0 {
-		*w4.DRAW_COLORS = 4
-	}
+	gameInstance.Update()
 
-	w4.Blit(&smiley[0], 76, 76, 8, 8, w4.BLIT_1BPP)
-	w4.Text("Press X to blink", 16, 90)
 }
